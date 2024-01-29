3ded7567e45447121e85ea450ef4d76492bad01f
LunarTides <31688109+LunarTides@users.noreply.github.com>
2024-01-29 14:19:15 +0100

    Temple: Add particles and camera shake

---
+++ Temple/Scripts/camera.gd
@@ -0,0 +1,26 @@
+extends Camera2D
+
+var is_shaking: bool = false
+
+func _on_gravestone_shake(amount, times, timer):
+	if is_shaking:
+		return
+	
+	# Shake the camera a bit
+	shake(0, amount, times, timer)
+
+func shake(depth, amount, times, timer):
+	# If it has shaked enough times, leave
+	if depth > times:
+		is_shaking = false
+		offset = Vector2.ZERO
+		return
+	
+	is_shaking = true
+	
+	# Move the camera  a little bit to the right, then move it a little to the left.
+	# Repeat until depth = shake_times
+	var amount_to_shake = (amount if depth & 1 == 0 else -amount) * randi_range(1, 2)
+	
+	offset += Vector2(amount_to_shake * [1, -1].pick_random(), amount_to_shake * [1, -1].pick_random())
+	get_tree().create_timer(timer).timeout.connect(func(): shake(depth + 1, amount, times, timer))
--- Temple/Scripts/gravestone.gd
+++ Temple/Scripts/gravestone.gd
@@ -7,27 +7,45 @@ var is_shaking = false
 @export var shake_amount: int = 5
 @export var shake_times: int = 5
 @export var shake_timer: float = 0.05
+@export var particle_timer: float = 0.1
+
+signal shake(shake_amount: int, shake_times: int, shake_timer: float)
 
 func _on_button_button_down():
+	# If it's already shaking, don't start shaking again
 	if is_shaking:
 		return
 	
+	# Play particles (It is a one shot)
+	$HitParticles.restart()
+	
+	# Play sound effect
 	$Hit.pitch_scale = randf_range(0.5, 1.0)
 	$Hit.play()
+	
+	# Internal counter
 	click_times += 1
 	
+	# If the player clicked enough times, close the game
 	if click_times >= click_amount:
 		get_tree().quit()
 		return
 	
-	shake()
+	do_shake()
 
-func shake(depth: int = 0):
+func do_shake(depth: int = 0):
+	# If it has shaked enough times, leave
 	if depth > shake_times:
 		is_shaking = false
 		return
 	
+	# Emit the shake signal
+	if depth == 0:
+		shake.emit(shake_amount, shake_times, shake_timer)
+	
 	is_shaking = true
 	
+	# Move the gravestone a little bit to the right, then move it a little to the left.
+	# Repeat until depth = shake_times
 	position.x += shake_amount if depth & 1 == 0 else -shake_amount
-	get_tree().create_timer(shake_timer).timeout.connect(func(): shake(depth + 1))
+	get_tree().create_timer(shake_timer).timeout.connect(func(): do_shake(depth + 1))
